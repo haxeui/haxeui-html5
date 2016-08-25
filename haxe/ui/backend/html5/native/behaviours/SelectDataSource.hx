09a22a528862b51435ba98348fe5a8fb2262797d
Ian Harrigan <ianharrigan@hotmail.com>
2016-08-25 15:57:47 +0200

    native dropdowns

---
+++ haxe/ui/backend/html5/native/behaviours/SelectDataSource.hx
@@ -0,0 +1,28 @@
+package haxe.ui.backend.html5.native.behaviours;
+
+import haxe.ui.core.Behaviour;
+import haxe.ui.data.DataSource;
+import haxe.ui.util.Variant;
+import js.html.Element;
+import js.html.OptionElement;
+import js.Browser;
+
+@:keep
+class SelectDataSource extends Behaviour {
+    public override function set(value:Variant) {
+        var ds:DataSource<Dynamic> = Variant.toDynamic(value);
+        var el:Element = _component.element;
+        while (el.childElementCount > 0) {
+            el.removeChild(el.children[0]);
+        }
+        
+        for (n in 0...ds.size) {
+            var item = ds.get(n);
+            if (item.text != null) {
+                var option:OptionElement = Browser.document.createOptionElement();
+                option.text = item.text;
+                el.appendChild(option);
+            }
+        }
+    }
+}
