51c5e6e6e454ae7cf8fefbe4eae087bebdc17b02
Ian Harrigan <ianharrigan@hotmail.com>
2017-01-20 20:56:53 +0100

    initial disabled for native html5 components

---
+++ haxe/ui/backend/html5/native/behaviours/ElementDisabled.hx
@@ -0,0 +1,22 @@
+package haxe.ui.backend.html5.native.behaviours;
+
+import haxe.ui.core.Behaviour;
+import haxe.ui.util.Variant;
+import js.html.Element;
+
+@:keep
+class ElementDisabled extends Behaviour {
+    public override function set(value:Variant) {
+        var el:Element = _component.element;
+        if (value == true) {
+            el.setAttribute("disabled", "true");
+        } else {
+            el.removeAttribute("disabled");
+        }
+    }
+    
+    public override function get():Variant {
+        var el:Element = _component.element;
+        return (el.getAttribute("disabled") == "true");
+    }
+}
