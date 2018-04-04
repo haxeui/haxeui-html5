378ff103f46fc580d726c61999053de0dd2cf8fd
Ian Harrigan <ianharrigan@hotmail.com>
2018-04-04 17:04:01 +0200

    use databehaviour

--- haxe/ui/backend/html5/native/behaviours/ElementAttribute.hx
+++ haxe/ui/backend/html5/native/behaviours/ElementAttribute.hx
@@ -1,14 +1,11 @@
 package haxe.ui.backend.html5.native.behaviours;
 
-import haxe.ui.core.Behaviour;
-import haxe.ui.util.Variant;
+import haxe.ui.core.DataBehaviour;
 import js.html.Element;
-import js.html.InputElement;
-import js.html.ProgressElement;
 
 @:keep
-class ElementAttribute extends Behaviour {
-    public override function set(value:Variant) {
+class ElementAttribute extends DataBehaviour {
+    public override function validateData() {
         var el:Element = _component.element;
         var name:String = getConfigValue("name");
         if (name == null) {
@@ -18,86 +15,18 @@ class ElementAttribute extends Behaviour {
         el = HtmlUtils.namedChild(el, getConfigValue("child"));
 
         if (getConfigValueBool("remove", false) == true) {
-            if (value == true) {
+            if (_value == true) {
                 el.removeAttribute(name);
             }
             return;
         }
-
-        /*
-        if (el.nodeName == "INPUT" && value != null) {
-            var input:InputElement = cast el;
-            if (value.isBool == true && name == "checked") {
-                input.checked = value;
-            } else if (name == "min") {
-                input.min = value;
-            } else if (name == "max") {
-                input.max = value;
-            } else if (name == "value") {
-                //input.value = value.toString();
-                Reflect.setProperty(input, "value", value.toString());
-                
-            }
-        } else if (el.nodeName == "PROGRESS" && value != null) {
-            var progress:ProgressElement = cast el;
-            if (name == "min") {
-                //progress.min = value;
-            } else if (name == "max") {
-                progress.max = value;
-            } else if (name == "value") {
-                progress.value = value;
-            }
-        } else {
-            el.setAttribute(name, value);
-        }
-        */
         
-        el.setAttribute(name, value);
+        el.setAttribute(name, _value);
         
         var removeIfNegative:Bool = getConfigValueBool("removeIfNegative", false);
-        if ((value == null || (value.isBool == true && value == false)) && removeIfNegative == true) {
+        if ((_value == null || (_value.isBool == true && _value == false)) && removeIfNegative == true) {
             el.removeAttribute(name);
             return;
         }
-    }
-    
-    public override function get():Variant {
-        var el:Element = _component.element;
-        var name:String = getConfigValue("name");
-        if (name == null) {
-            return null;
-        }
-
-        el = HtmlUtils.namedChild(el, getConfigValue("child"));
-        
-        var value:Variant = null;
-        
-        /*
-        if (el.nodeName == "INPUT") {
-            var input:InputElement = cast el;
-            if (name == "checked") {
-                value = input.checked;
-            } else if (name == "min") {
-                value = input.min;
-            } else if (name == "max") {
-                value = input.max;
-            } else if (name == "value") {
-                value = input.value;
-            }
-        } else if (el.nodeName == "PROGRESS") {
-            var progress:ProgressElement = cast el;
-            if (name == "min") {
-                //value = progress.min;
-            } else if (name == "max") {
-                value = progress.max;
-            } else if (name == "value") {
-                value = progress.value;
-            }
-        } else {
-            value = el.getAttribute(name);
-        }
-        */
-        value = el.getAttribute(name);
-        return value;
     }
 }
