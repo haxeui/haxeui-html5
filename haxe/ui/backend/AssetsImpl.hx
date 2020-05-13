491dbb8f806a016b62aafb3b532ef5b0be65b6e9
Ian Harrigan <ianharrigan@hotmail.com>
2020-05-13 23:14:52 +0200

    handle fonts differently

--- haxe/ui/backend/AssetsImpl.hx
+++ haxe/ui/backend/AssetsImpl.hx
@@ -6,6 +6,7 @@ import haxe.ui.assets.ImageInfo;
 import haxe.ui.backend.html5.util.FontDetect;
 import js.Browser;
 import js.html.Blob;
+import js.html.FontFace;
 import js.html.URL;
 
 class AssetsImpl extends AssetsBase {
@@ -69,13 +70,54 @@ class AssetsImpl extends AssetsBase {
     }
     
     private override function getFontInternal(resourceId:String, callback:FontInfo->Void) {
-        FontDetect.onFontLoaded(resourceId, function(f) {
-            var fontInfo = {
-                data: f
-            }
-            callback(fontInfo);
-        }, function(f) {
-            callback(null);
+        var bytes = Resource.getBytes(resourceId);
+        if (bytes == null) {
+            FontDetect.onFontLoaded(resourceId, function(f) {
+                var fontInfo = {
+                    data: f
+                }
+                callback(fontInfo);
+            }, function(f) {
+                callback(null);
+            });
+            return;
+        }
+
+        getFontFromHaxeResource(resourceId, function(r, f) {
+            callback(f);
         });
+    }
+    
+    private override function getFontFromHaxeResource(resourceId:String, callback:String->FontInfo->Void) {
+        var bytes = Resource.getBytes(resourceId);
+        if (bytes == null) {
+            callback(resourceId, null);
+            return;
+        }
+        
+        var fontFamilyParts = resourceId.split("/");
+        var fontFamily = fontFamilyParts[fontFamilyParts.length - 1];
+        if (fontFamily.indexOf(".") != -1) {
+            fontFamily = fontFamily.substr(0, fontFamily.indexOf("."));
+        }
+        
+        var fontFace = new FontFace(fontFamily, bytes.getData());
+        fontFace.load().then(function(loadedFace) {
+            Browser.document.fonts.add(loadedFace);
+            FontDetect.onFontLoaded(fontFamily, function(f) {
+                var fontInfo = {
+                    data: fontFamily
+                }
+                callback(resourceId, fontInfo);
+            }, function(f) {
+                callback(resourceId, null);
+            });
+        }).catchError(function(error) {
+            #if debug
+            trace("WARNING: problem loading font '" + resourceId + "' (" + error + ")");
+            #end
+			// error occurred
+            callback(resourceId, null);
+		});
     }
 }
