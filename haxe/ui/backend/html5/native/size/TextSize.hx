1641ce0e1840a3d0ec28885a2e824efce4f5c05e
Ian Harrigan <ianharrigan@hotmail.com>
2020-08-25 10:17:54 +0200

    fix native textsize

--- haxe/ui/backend/html5/native/size/TextSize.hx
+++ haxe/ui/backend/html5/native/size/TextSize.hx
@@ -5,12 +5,20 @@ import haxe.ui.layouts.DelegateLayout.DelegateLayoutSize;
 @:keep
 class TextSize extends DelegateLayoutSize {
     private override function get_width():Float {
-        var size = HtmlUtils.measureText(component.text, 0, 0, component.style.fontSize, component.style.fontName);
+        var text = component.text;
+        if (text == null) {
+            text = "";
+        }
+        var size = HtmlUtils.measureText(text, 0, 0, component.style.fontSize, component.style.fontName);
         return size.width + getInt("incrementWidthBy");
     }
 
     private override function get_height():Float {
-        var size = HtmlUtils.measureText(component.text, 0, 0, component.style.fontSize, component.style.fontName);
+        var text = component.text;
+        if (text == null) {
+            text = "|";
+        }
+        var size = HtmlUtils.measureText(text, 0, 0, component.style.fontSize, component.style.fontName);
         return size.height + getInt("incrementHeightBy");
     }
 }
