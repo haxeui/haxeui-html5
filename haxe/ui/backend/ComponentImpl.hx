c8ef6ef0320faaeedc20d453495297048619a836
Ian Harrigan <ianharrigan@hotmail.com>
2022-02-12 10:57:19 +0100

    tweaks

--- haxe/ui/backend/ComponentImpl.hx
+++ haxe/ui/backend/ComponentImpl.hx
@@ -226,6 +226,7 @@ class ComponentImpl extends ComponentBase {
     private override function handleClipRect(value:Rectangle) {
         var c:Component = cast(this, Component);
         var parent:Component = c.parentComponent;
+        value.toInts();
         if (value != null && parent != null) {
             element.style.clip = 'rect(${HtmlUtils.px(value.top)},${HtmlUtils.px(value.right - 1)},${HtmlUtils.px(value.bottom)},${HtmlUtils.px(value.left)})';
             element.style.left = '${HtmlUtils.px(Std.int(c.left - value.left))}';
@@ -605,11 +606,13 @@ class ComponentImpl extends ComponentBase {
                 }
                 
                 // js dom events fire mouse outs when you mouse over a child, lets fix that
+                /*
                 if (type == MouseEvent.MOUSE_OUT && hitTest(mouseEvent.screenX, mouseEvent.screenY) == true) {
                     return;
                 } else if (type == MouseEvent.MOUSE_OVER && _over == true) {
                     return;
                 }
+                */
                 
                 if (type == MouseEvent.MOUSE_OVER) {
                     _over = true;
